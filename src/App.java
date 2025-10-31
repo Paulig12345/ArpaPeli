b5a9adc3a45a80826dcda2e8fdf22b537ccbfbc7
Paulig12345 <pauli.pynnonen@student.hamk.fi>
2025-10-31 10:27:32 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      
+      // tehdään muuttujat
         int numero = 2;
         int numero1 = 3;
       Scanner in = new Scanner(System.in);
-
+          // kysytään ensimmäistä numeroa
          System.out.println("First number?");
           numero = in.nextInt();{
           }
-
+         // kysytään toista numeroa
         System.out.println("Second number?");
           numero1 = in.nextInt();
         
          int sum = (numero + numero1);
           
-
+         // printataan vastaus
         System.out.println("The sum is. " + sum + "." );
 
 
